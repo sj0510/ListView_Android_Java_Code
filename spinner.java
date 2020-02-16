1c519a9af03041fac2718dde8aa1e003e6fafca6
$j <42267783+sj932@users.noreply.github.com>
2020-02-16 15:55:36 +0530

    Create spinner.java

---
+++ spinner.java
@@ -0,0 +1,26 @@
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner_days);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.days, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+
+    }
